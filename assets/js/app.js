3a914b7922d5b9c1a19e7cab742d99202d4d64f4
tom0-shibi <tom0shibi.7553@gmail.com>
2026-09-22 15:13:45 +0900

    feat: refresh checker series UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,7 @@ import { renderAnalysisDebug, renderAnalysisError, renderUnsupportedLayouts, ren
 import { ensureDynamicStyles, ensureAnalysisProgressOverlay, ensureResultSummaryContainer, updateAnalysisProgressDisplay, showAnalysisProgress, hideAnalysisProgress, showAnalysisCompleteProgress, scrollToResultsTop, initializePageScrollPosition, setPasteTarget, updateImageSummary } from "./ui/ui.js?v=20260921-duplicate-paste-01";
 import { initializePresetManager } from "./preset/preset-manager.js?v=20260921-duplicate-paste-01";
 import { initializeTheme } from "./ui/theme.js";
+import { initializeUsageModal } from "./ui/usage-modal.js?v=20260922-series-ui-01";
 import { initializeRepresentativeCheck } from "./representative/representative-check.js?v=20260917-factor-master-01";
 
 console.info(
@@ -29,6 +30,7 @@ if (IS_DEV) {
 }
 
 initializeTheme();
+initializeUsageModal();
 initializePageScrollPosition();
 initializePresetManager();
 initializeRepresentativeCheck();
